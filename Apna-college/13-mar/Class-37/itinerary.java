6efa031db20cd99b28c54647b041a844d37d8424
jainam-15 <jainam150606@gmail.com>
2026-03-14 14:44:53 +0530

    Initial Commit

---
+++ Apna-college/13-mar/Class-37/itinerary.java
@@ -0,0 +1,38 @@
+
+import java.util.*;
+
+public class itinerary {
+
+    public static String getstart(HashMap<String, String> tick) {
+        HashMap <String, String> revmap = new HashMap<>();
+
+        for(String key : tick.keySet()) {
+            revmap.put(tick.get(key), key);
+        }
+
+        for(String key : tick.keySet()) {
+            if(!revmap.containsKey(key)) {
+                return key;
+            }
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        HashMap<String, String> tickets = new HashMap<>();
+
+        tickets.put("Chennai", "Bengaluru");
+        tickets.put("Mumbai", "Delhi");
+        tickets.put("Goa", "Chennai");
+        tickets.put("Delhi", "Goa");
+
+        String start = getstart(tickets);
+
+        while(tickets.containsKey(start)) {
+            System.out.print(start + " -> ");
+            start = tickets.get(start);
+        }
+        System.out.println(start);
+    }
+}
---
+++ Apna-college/13-mar/Class-37/union.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class union {
+
+    public static void union(int arr1[], int arr2[]) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < arr1.length; i++) {
+            set.add(arr1[i]);
+        }
+
+        for (int i = 0; i < arr2.length; i++) {
+            set.add(arr2[i]);
+        }
+
+        System.out.print(set);
+    }
+    public static void main(String[] args) {
+        int arr1[] = {7, 3, 9};
+        int arr2[] = {6, 3, 9, 2, 9, 4};
+        union(arr1, arr2);
+        
+    }
+}
